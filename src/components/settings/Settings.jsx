ccf12f8716f721ac8a47979cf5a7938af113a5e7
Ilhom <ilhom@example.com>
2024-07-12 11:45:56 +0500

    homework

--- src/components/settings/Settings.jsx
+++ src/components/settings/Settings.jsx
@@ -14,89 +14,89 @@ const Settings = () => {
         >
             <Row gutter={16}>
                 <Col span={10}>
-                    <Form.Item label="Тип" name="type">
+                    <Form.Item label="Тип" name="type" rules={[{ required: true, message: 'Заполнить Тип!' }]}>
                         <Input placeholder="К какой из категорий ниже больше всего подходит ваш объект?"/>
                     </Form.Item>
                 </Col>
                 <Col span={10}>
-                    <Form.Item label="Название" name="name">
+                    <Form.Item label="Название" name="name" rules={[{ required: true, message: 'Заполнить Название!' }]}>
                         <Input placeholder="Как называется ваша гостиница?"/>
                     </Form.Item>
                 </Col>
             </Row>
             <Row gutter={16}>
                 <Col span={10}>
-                    <Form.Item label="Звездность" name="stars">
+                    <Form.Item label="Звездность" name="stars" rules={[{ required: true, message: 'Заполнить Звездность!' }]}>
                         <Input placeholder="Сколько звезд у вашей гостиницы?"/>
                     </Form.Item>
                 </Col>
                 <Col span={10}>
-                    <Form.Item label="Адрес" name="address">
+                    <Form.Item label="Адрес" name="address" rules={[{ required: true, message: 'Заполнить Адрес!' }]}>
                         <Input placeholder="Где находится ваша гостиница?"/>
                     </Form.Item>
                 </Col>
             </Row>
             <Row gutter={16}>
                 <Col span={7}>
-                    <Form.Item label="Номер телефона" name="phone">
+                    <Form.Item label="Номер телефона" name="phone" rules={[{ required: true, message: 'Заполнить Номер телефона!' }]}>
                         <Input placeholder="Введите номер телефона"/>
                     </Form.Item>
                 </Col>
                 <Col span={7}>
-                    <Form.Item label="Дополнительный номер телефона" name="additionalPhone">
+                    <Form.Item label="Дополнительный номер телефона" name="additionalPhone" rules={[{ required: true, message: 'Заполнить Дополнительный номер телефона!' }]}>
                         <Input placeholder="Введите дополнительный номер телефона"/>
                     </Form.Item>
                 </Col>
                 <Col span={7}>
-                    <Form.Item label="Факс" name="fax">
+                    <Form.Item label="Факс" name="fax" rules={[{ required: true, message: 'Заполнить ввод!' }]}>
                         <Input placeholder="Введите факс"/>
                     </Form.Item>
                 </Col>
             </Row>
             <Row gutter={16}>
                 <Col span={7}>
-                    <Form.Item label="Емаил" name="email">
+                    <Form.Item label="Емаил" name="email" rules={[{ required: true, message: 'Заполнить ввод!' }]}>
                         <Input placeholder="Введите email"/>
                     </Form.Item>
                 </Col>
                 <Col span={7}>
-                    <Form.Item label="Email отдела бронирования" name="bookingEmail">
+                    <Form.Item label="Email отдела бронирования" name="bookingEmail" rules={[{ required: true, message: 'Заполнить ввод!' }]}>
                         <Input placeholder="Введите email отдела бронирования"/>
                     </Form.Item>
                 </Col>
                 <Col span={7}>
-                    <Form.Item label="Почтовый индекс" name="postalCode">
+                    <Form.Item label="Почтовый индекс" name="postalCode" rules={[{ required: true, message: 'Заполнить ввод!' }]}>
                         <Input placeholder="Введите индекс"/>
                     </Form.Item>
                 </Col>
             </Row>
             <Row gutter={16}>
                 <Col span={7}>
-                    <Form.Item label="Время заезда" name="checkInTime">
+                    <Form.Item label="Время заезда" name="checkInTime" rules={[{ required: true, message: 'Заполнить ввод!' }]}>
                         <Input placeholder="Например: 08:00"/>
                     </Form.Item>
                 </Col>
                 <Col span={7}>
-                    <Form.Item label="Время выезда" name="checkOutTime">
+                    <Form.Item label="Время выезда" name="checkOutTime" rules={[{ required: true, message: 'Заполнить ввод!' }]}>
                         <Input placeholder="Например: 18:00"/>
                     </Form.Item>
                 </Col>
                 <Col span={7}>
-                    <Form.Item label="Форма оплаты" name="paymentMethod">
+                    <Form.Item label="Форма оплаты" name="paymentMethod" rules={[{ required: true, message: 'Заполнить ввод!' }]}>
                         <Input placeholder="Какие способы оплаты вы принимаете?"/>
                     </Form.Item>
                 </Col>
             </Row>
             <Row gutter={16}>
                 <Col span={22}>
-                    <Form.Item label="Услуги гостиницы" name="services">
+                    <Form.Item label="Услуги гостиницы" name="services" rules={[{ required: true, message: 'Заполнить ввод!' }]}>
                         <Input placeholder="Какие услуги предоставляет ваша гостиница клиентам?"/>
                     </Form.Item>
                 </Col>
             </Row>
             <Row gutter={16}>
                 <Col span={22}>
-                    <Form.Item label="Ближайшие достопримечательности и развлекательные центры" name="services">
+                    <Form.Item label="Ближайшие достопримечательности и развлекательные центры" name="services" rules={[{ required: true, message: 'Заполнить ввод!' }]}>
                         <Input placeholder="Ближайшие достопримечательности и развлекательные центры "/>
                     </Form.Item>
                 </Col>
